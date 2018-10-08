7e48f2640ada67c905499b825f15440fbcc1e9c4
barchi6223 <40294088+barchi6223@users.noreply.github.com>
2018-10-08 21:14:56 +0800

    Add files via upload
    演算法作業

---
+++ HW1005.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class HW1005{
+    public static void main(String[] argv){
+        int m,n,x=0,a=0,ans;
+        Scanner input = new Scanner(System.in);
+        System.out.println("俄式乘法");
+        System.out.print("請輸入m:");
+        m=input.nextInt();
+        System.out.print("請輸入n:");
+        n=input.nextInt();
+
+        System.out.println(m+"   "+n);
+        while(m!=1){
+            if (m%2 != 0) {
+                m=m/2; x+=n; a=n;
+            }else{
+                m=m/2; a=0;
+            }
+            n=n*2;
+            System.out.println(m+"   "+n+"   (+"+a+")");
+        }
+        ans=n+x;
+        System.out.print(n+"+"+x+"="+ans);
+
+    }
+}
